f4f2b6c6730ac6fddb34f9135a86cdd5b07e78c7
vsreddyh <shouryanreddyh@gmail.com>
2024-11-15 11:04:06 +0530

    init

---
+++ src/main/java/com/maven/demo/javamavenproject/App.java
@@ -0,0 +1,10 @@
+package com.maven.demo.javamavenproject;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello Vishnu");
+    }
+}
